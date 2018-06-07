f88b583ebb8d362ca056f67961c3324589ebda37
Tristou <triclem2@gmail.com>
2018-06-07 17:41:23 +0200

    save avant demain

--- php/index.php
+++ php/index.php
@@ -18,19 +18,20 @@
 
     <body>
         <?php /*include("./main_header.php") */?>
+
         <main class="mainIndex">
-            <h1>Bienvenue sur Filyso !</h1>
-            <section>
-                <h2>Qu'est-ce que Filsyo ?</h2>
-                <p>Filyso est né dans le cadre d'un projet étudiant en 2018. Il s'agit d'un site vous proposant quelques jeux basés sur les paroles de chansons. Les jeux sont répartis en modes de jeu, en solo ou en multijoueur.<br/> Notre application est développée au département MMI de l'IUT de Laval, en France. Le but est d'imaginer et de concevoir une application en rapport avec un certain sujet et présentant certaines fonctionnalités. La première version que nous vous présentons ici est un prototype dans lequel seul le mode de jeu en solo n'est disponible.</p>
-            </section>
-            <a href="./select_game.php">JOUER</a>
-            
-            <div class="divVideo">
+           <h1>FILYSO</h1>
+            <section class="divVideo">
                 <video autoplay muted loop id="video">
                     <source src="../videos/animation_principale.mp4" type="video/mp4">
                 </video>
-            </div>
+                <a href="./select_game.php">JOUER</a>
+            </section>
+            <section class="welcomeIndex">
+                <h2>Qu'est-ce que Filsyo ?</h2>
+                <p>Filyso est né dans le cadre d'un projet étudiant en 2018. Il s'agit d'un site vous proposant quelques jeux basés sur les paroles de chansons. Les jeux sont répartis en modes de jeu, en solo ou en multijoueur.<br/> Notre application est développée au département MMI de l'IUT de Laval, en France. Le but est d'imaginer et de concevoir une application en rapport avec un certain sujet et présentant certaines fonctionnalités. La première version que nous vous présentons ici est un prototype dans lequel seul le mode de jeu en solo n'est disponible.</p>
+            </section>
+
         </main>
 
         <?php /*include("./main_footer.php")*/?>
