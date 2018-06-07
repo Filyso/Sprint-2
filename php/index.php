656e997673a9298b8bd388b418d6f8baf3190aa9
Tristou <triclem2@gmail.com>
2018-06-07 16:07:14 +0200

    Save on sait jamais

--- php/index.php
+++ php/index.php
@@ -11,13 +11,13 @@
         <meta http-equiv="" content="">
         <title></title>
         <meta name="" content="">
-        <link rel="stylesheet" href="../style.css">
+        <link rel="stylesheet" href="../css/style_index.css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:600,900" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,500,700" rel="stylesheet">
     </head>
 
     <body>
-        <?php include("./main_header.php")?>
+        <?php /*include("./main_header.php") */?>
         <main class="mainIndex">
             <h1>Bienvenue sur Filyso !</h1>
             <section>
@@ -25,9 +25,15 @@
                 <p>Filyso est né dans le cadre d'un projet étudiant en 2018. Il s'agit d'un site vous proposant quelques jeux basés sur les paroles de chansons. Les jeux sont répartis en modes de jeu, en solo ou en multijoueur.<br/> Notre application est développée au département MMI de l'IUT de Laval, en France. Le but est d'imaginer et de concevoir une application en rapport avec un certain sujet et présentant certaines fonctionnalités. La première version que nous vous présentons ici est un prototype dans lequel seul le mode de jeu en solo n'est disponible.</p>
             </section>
             <a href="./select_game.php">JOUER</a>
+            
+            <div class="divVideo">
+                <video autoplay muted loop id="video">
+                    <source src="../videos/animation_principale.mp4" type="video/mp4">
+                </video>
+            </div>
         </main>
 
-        <?php include("./main_footer.php")?>
+        <?php /*include("./main_footer.php")*/?>
     </body>
 
     </html>
