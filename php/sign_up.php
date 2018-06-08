ee61a7a21d05c8cf1495cc000f9b210071ce2ff6
Unknown <villatte.maxime@orange.fr>
2018-06-08 13:35:04 +0200

    pause

--- php/sign_up.php
+++ php/sign_up.php
@@ -1,5 +1,43 @@
 <?php
-    session_start();
+
+session_start();
+
+if(isset($_POST["pseudoSignUp"]) && isset($_POST["lastname"]) && isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["passSignUp"]) && isset($_POST["passverif"]) && ($_POST["passSignUp"] == $_POST["passverif"])){
+    
+    
+    require("./param.inc.php");
+    $pdo = new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS);
+    $pdo->query("SET NAMES utf8");
+    $pdo->query("SET CHARACTER SET 'utf8'");
+
+    $requeteSQL = "SELECT pseudoMbr, mailMbr FROM MEMBRES WHERE UPPER(pseudoMbr)= UPPER(:pseudo) OR mailMbr= :email";
+    $statement = $pdo->prepare($requeteSQL);
+    $statement->execute(array(":pseudo" => $_POST["pseudoSignUp"], ":email" => $_POST["email"]));
+    
+    $ligne = $statement->fetch(PDO::FETCH_ASSOC);
+    
+    echo($ligne["pseudoMbr"]."  ".$ligne["mailMbr"]."<br/>");
+    echo($_POST["pseudoSignUp"]."  ".$_POST["email"]."<br/>");
+    
+    // le pseudo ou le mail ne doit pas être déjà utilisé
+    if(!empty($ligne["pseudoMbr"]) || !empty($ligne["mailMbr"])){
+        //inscription invalide
+        echo("Pseudo ou mail invalide");
+    }else{
+        //inscription valide
+        $requeteSQL = "INSERT INTO `MEMBRES` (`pseudoMbr`, `nameMbr`, `prenomMbr`, `mailMbr`, `mdpMbr`,  `isVerif`) VALUES ('1', '-èu', '-èu', '-èu', '-èu', '-èu', '-èu', '0')
+";
+    }
+
+
+    $pdo = null;
+    
+    
+    
+}else{
+    echo("C PA BN"); 
+}
+
 ?>
 
 
@@ -18,70 +56,35 @@
 <body>
     <?php include("./main_header.php"); ?>
 
-    <main class="preGamePage">
+    <main class="signUpPage">
         <h1>Sélectionnez la langue et la catégorie</h1>
-        <form action="./solo_game.php" method="get">
-
-            <fieldset class="section">
-                <div class="container">
-
-                    <input type="radio" name="langue" id="français" value="fr" class="inputRadio" required />
-                    <label for="français"><img src="../images/FR.png" alt="drapeau français"></label>
-                </div>
-
-                <div class="container">
-
-                    <input type="radio" name="langue" id="anglais" value="en" class="inputRadio" />
-                    <label for="anglais"><img src="../images/UK.png" alt="drapeau anglais"></label>
-                </div>
-
-                <div class="container">
-
-                    <input type="radio" name="langue" id="bilingue" value="bilingue" class="inputRadio" checked/>
-                    <label for="bilingue"><img src="../images/FRUK.png" alt="drapeau bilingue"></label>
-                </div>
-            </fieldset>
-
+        <form action="./sign_up.php" method="post"  style="margin-top:200px">
             <fieldset>
                 <div>
-                    <label for="categorie">Catégorie</label>
-                    <select id="categorie" name="categorie">
-                        <option value="0">Toutes les catégories</option>
-                        <?php
-                            try {
-                            // ETAPE 1 : Se connecter au serveur de base de données
-                                require("./param.inc.php");
-                                $pdo = new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS);
-                                $pdo->query("SET NAMES utf8");
-                                $pdo->query("SET CHARACTER SET 'utf8'");
+                    <label for="pseudoSignUp">Pseudo</label>
+                    <input name="pseudoSignUp" type="text" id="pseudoSignUp" required="required"></input>
+                
+                    <label for="lastname">Nom</label>
+                    <input name="lastname" type="text" id="lastname" required="required"></input>
+            
+                    <label for="name">Prénom</label>
+                    <input name="name" type="text" id="name" required="required"></input>
+        
+                    <label for="email">Mail</label>
+                    <input name="email" type="email" id="email" required="required"></input>
+    
+                    <label for="passSignUp">Mot de passe</label>
+                    <input name="passSignUp" type="password" id="passSignUp" required="required"></input>
+    
+                    <label for="passverif">Vérification du mot de passe</label>
+                    <input name="passverif" type="password" id="passverif" required="required"></input>
 
-                            // ETAPE 2 : Envoyer une requête SQL (demander la liste des données)
-                                $requeteSQL = "SELECT idCat, nameCat FROM CATEGORIES";
-                                $statement = $pdo->query($requeteSQL);
 
-                            // ETAPE 3 : Traiter les données retourner
-                                $ligne = $statement->fetch(PDO::FETCH_ASSOC);
-                                while($ligne != false) {
-                        ?>
-                        <option value="<?php echo($ligne["idCat"]);?>"><?php echo(ucfirst($ligne["nameCat"]));?></option>
-                        <?php
-                                    $ligne = $statement->fetch(PDO::FETCH_ASSOC);
-                                }
-                            // Fin de la boucle
-                            // ETAPE 4 : Déconnecter du serveur
-                                $pdo = null;
-                            } catch (Exception $e) {
-                                echo($e);
-                            }
-                        ?>
-                    </select>
-                </div>
-
-                <div>
-                    <button id="btnAlea" type="button">Aléatoire</button>
+                    <!-- <label for="iconPath">Choississez une icône de joueur !</label>
+                    <input name="iconPath" type="file" id="iconPath" required="required"></input> -->                 
                 </div>
             </fieldset>
-            <button type="submit">Jouer</button>
+            <button type="submit">Terminer</button>
         </form>
     </main>
     <?php include("./main_footer.php"); ?>
