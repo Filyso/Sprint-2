816e9fadb0ee8734a2da083c217f9f5377e66300
Tristou <triclem2@gmail.com>
2018-06-08 12:03:41 +0200

    save before manger

--- php/index.php
+++ php/index.php
@@ -20,13 +20,12 @@
         <?php /*include("./main_header.php") */?>
 
         <main class="mainIndex">
-           <h1>FILYSO</h1>
-            <section class="divVideo">
-                <video autoplay muted loop id="video">
-                    <source src="../videos/animation_principale.mp4" type="video/mp4">
-                </video>
+            <h1>FILYSO</h1>
+            
+            <section class="indexLink">
                 <a href="./select_game.php">JOUER</a>
             </section>
+
             <section class="welcomeIndex">
                 <h2>Qu'est-ce que Filsyo ?</h2>
                 <p>Filyso est né dans le cadre d'un projet étudiant en 2018. Il s'agit d'un site vous proposant quelques jeux basés sur les paroles de chansons. Les jeux sont répartis en modes de jeu, en solo ou en multijoueur.<br/> Notre application est développée au département MMI de l'IUT de Laval, en France. Le but est d'imaginer et de concevoir une application en rapport avec un certain sujet et présentant certaines fonctionnalités. La première version que nous vous présentons ici est un prototype dans lequel seul le mode de jeu en solo n'est disponible.</p>
