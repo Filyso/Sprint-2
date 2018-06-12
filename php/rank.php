589352bce154327c74b37e3d2ef80b5fb015bb22
Unknown <vscavinner@gmail.com>
2018-06-12 10:55:37 +0200

    classement V1
    Fin (en attente de test pour vérification)

--- php/rank.php
+++ php/rank.php
@@ -10,134 +10,83 @@
         <meta http-equiv="" content="">
         <title></title>
         <meta name="" content="">
-<!--        <link rel="stylesheet" href="../style.css">-->
+        <!--        <link rel="stylesheet" href="../style.css">-->
         <link rel="stylesheet" href="../css/style_index.css">
         <link rel="stylesheet" href="../css/style_rank.css">
     </head>
 
     <body>
         <?php //include("./main_header.php")?>
-        <main class="mainRank">
-            <h1>Classement des joueurs</h1>
+            <main class="mainRank">
+                <h1>Classement des joueurs</h1>
                 <table>
                     <tr>
                         <th>Joueur</th>
                         <th>Score</th>
                         <th>Classement</th>
                     </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>Michel</p>
-                        </td>
-                        <td>10.000</td>
-                        <td>1</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>Samuel</p>
-                        </td>
-                        <td>9.000</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>Francis</p>
-                        </td>
-                        <td>8.000</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>Antonin54</p>
-                        </td>
-                        <td>7.000</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>ElRodriguo</p>
-                        </td>
-                        <td>6.000</td>
-                        <td>5</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>MasutaSama</p>
-                        </td>
-                        <td>5.000</td>
-                        <td>6</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>Philippe</p>
-                        </td>
-                        <td>4.000</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>Duschmol</p>
-                        </td>
-                        <td>3.000</td>
-                        <td>8</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>Dudule</p>
-                        </td>
-                        <td>2.000</td>
-                        <td>9</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>TontonJC</p>
-                        </td>
-                        <td>1.000</td>
-                        <td>10</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <figure>
-                                <img  alt="Photo de profil joueur" src="../images/chat.jpg"/>
-                            </figure>
-                            <p>Toi</p>
-                        </td>
-                        <td>870</td>
-                        <td>22</td>
-                    </tr>
+
+                    <?php
+                    
+	                   //Etape 1 : connection à la base de données
+		
+                        require("param.inc.php") ;
+                        $pdo=new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS) ;
+                        $pdo->query("SET NAMES utf8");
+                        $pdo->query("SET CHARACTER SET 'utf8'");
+
+                        //Etape 2 : envoie la requête SQL au serveur
+
+                        $requeteSQL="SELECT membres.pseudoMbr AS 'nom', membres.linkIconMbr AS 'lien', COUNT(joue.score) AS 'score' ".
+                                    "FROM membres ".
+                                    "INNER JOIN joue ".
+                                    "ON membres.idMbr = joue.idMbr ".
+                                    "LIMIT 10 ".
+                                    "ORDER BY joue.score DESC";
+
+                        $tabParam = array();
+
+                        $statement = $pdo->prepare($requeteSQL) ;
+                        $statement->execute($tabParam);
+
+                        // Etape 3 : traite les données
+                        
+                        $currentPosition = 1;
+                    
+                        //Premier membre
+                        $ligne = $statement->fetch(PDO::FETCH_OBJ);
+
+                        //Boucle sur chaque membre (BDD)
+                        while($ligne != false){
+	               ?>
+
+                        <tr>
+                            <td>
+                                <figure>
+                                    <img alt="Photo de profil joueur" src="<?php echo($ligne->lien) ?>" />
+                                </figure>
+                                <p><?php echo($ligne->nom) ?></p>
+                            </td>
+                            <td><?php echo($ligne->score) ?></td>
+                            <td> <?php echo($currentPosition) ?></td>/*Comment faaaaaaaaaaaaaaaaaaaaaaaire ?!*/
+                        </tr>
+                    <?php
+                            //Membre suivant
+                       
+                            $currentPosition += 1; 
+                            
+                            $ligne = $statement->fetch(PDO::FETCH_OBJ);
+                            }
+                    
+                            //Fin boucle
+
+                            // Etape 4 : ferme la connexion
+
+                            $pdo = null;
+                     ?>
                 </table>
-        </main>
-        <?php include("./main_footer.php")?>
+            </main>
+            <?php include("./main_footer.php")?>
     </body>
 
     </html>
