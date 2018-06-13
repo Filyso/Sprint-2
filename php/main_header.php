7a017fbf7059c56ec4b056c3485c9421b0a99d1f
Tristou <triclem2@gmail.com>
2018-06-13 12:02:47 +0200

    save2

--- php/main_header.php
+++ php/main_header.php
@@ -36,33 +36,33 @@
 
 
 ?>
-        <header class="mainHeader">
+    <header class="mainHeader">
 
-            <!-- Icône Menu Burger -->
-            <div class="container">
-                <svg>
+        <!-- Icône Menu Burger -->
+        <div class="container">
+            <svg>
                     <path id="first" d="M 30 40 L 70 40 C 90 40 90 75 60 85 A 40 40 0 0 1 20 20 L 80 80"></path>
                     <path id="middle" d="M 30 50 L 70 50"></path>
                     <path id="second" d="M 70 60 L 30 60 C 10 60 10 20 40 15 A 40 38 0 1 1 20 80 L 80 20"></path>
                 </svg>
-            </div>
+        </div>
 
-            <nav class="menuHidden">
+        <nav class="menuHidden">
 
-                <ul>
+            <ul>
 
-                    <li>
-                        <a href="index.php">ACCUEIL</a>
-                    </li>
+                <li>
+                    <a href="index.php">ACCUEIL</a>
+                </li>
 
-                    <li>
-                        <a href="./select_game.php">JOUER</a>
-                    </li>
+                <li>
+                    <a href="./select_game.php">JOUER</a>
+                </li>
 
-                    <li>
-                        <a href="rank.php">CLASSEMENT</a>
-                    </li>
-<?php
+                <li>
+                    <a href="rank.php">CLASSEMENT</a>
+                </li>
+                <?php
     
     if(Membre::isLogged()){               
 ?>
@@ -70,28 +70,30 @@
                         <a href="logout.php">DÉCONNEXION</a>
                     </li>
 
-<?php
+                    <?php
     }else{
 ?>
-                    <li>
-                        <p class="loginMenu">CONNEXION</p>
-                    </li>                  
-<?php
+                        <li>
+                            <p class="loginMenu">CONNEXION</p>
+                        </li>
+                        <?php
     }
 ?>
-                </ul>
-            </nav>
+            </ul>
+        </nav>
 
-            <div class="rightHeader">
+        <div class="rightHeader">
 
-                <div class="connect">
-                    <p class="login">CONNEXION</p>
-                </div>
-                <div class="connect">
-                    <a href="inscription.php" class="signup"><p class="login">INSCRIPTION</p></a>
-                </div>
-                <div id="fullScreenButton">
-                    <svg viewBox="0 0 24 24">
+            <div class="connect">
+                <p class="login">CONNEXION</p>
+            </div>
+            <div class="connect">
+                <a href="inscription.php" class="signup">
+                    <p class="login">INSCRIPTION</p>
+                </a>
+            </div>
+            <div id="fullScreenButton">
+                <svg viewBox="0 0 24 24">
 
                         <path id="play" d="m 3.4285714,15.428571 -3.42857145,0 0,8.571429 8.57142905,0 0,-3.428571 -5.1428577,0 0,-5.142858 z M -5e-8,8.5714287 l 3.42857145,0 0,-5.1428573 5.1428577,0 L 8.5714291,0 -4.9999999e-8,0 l 0,8.5714287 z M 20.571428,20.571429 l -5.142857,0 0,3.428571 L 24,24 l 0,-8.571429 -3.428572,0 0,5.142858 z M 15.428571,2e-7 l 0,3.4285714 5.142857,0 0,5.1428571 3.428572,0 L 24,2e-7 l -8.571429,0 z">
 
@@ -102,25 +104,25 @@
 
                         </path>
                     </svg>
-                </div>
-
             </div>
-            <div class="popup">
-                <div class="popup-content">
-                    <span class="close">&times;</span>
-                    <h2>CONNECTEZ-VOUS</h2>
-                    <a href="sign_up.php">inscrivez-vous</a>
-                    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="post">
-                        <input id="pseudo" type="text" required="required" name="pseudo" placeholder="Pseudo...">
-                        <input id="passwd" type="password" required="required" name="pass" placeholder="Mot de passe...">
-                        <button type="submit">SE CONNECTER</button>
-                    </form>
-                </div>
-            </div>
-        </header>
 
-        <script src="https://kodhus.com/moveit.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="../javascript/popup_login.js"></script>
-        <script type="text/javascript" src="../javascript/main_header.js"></script>
+        </div>
+        <div class="popup">
+            <div class="popup-content">
+                <span class="close">&times;</span>
+                <h2>CONNECTEZ-VOUS</h2>
+                <a href="sign_up.php">inscrivez-vous</a>
+                <form action="<?php echo($_SERVER[" SCRIPT_NAME "]);?>" method="post">
+                    <input id="pseudo" type="text" required="required" name="pseudo" placeholder="Pseudo...">
+                    <input id="passwd" type="password" required="required" name="pass" placeholder="Mot de passe...">
+                    <button type="submit">SE CONNECTER</button>
+                </form>
+            </div>
+        </div>
+    </header>
+
+    <script src="https://kodhus.com/moveit.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="../javascript/popup_login.js"></script>
+    <script type="text/javascript" src="../javascript/main_header.js"></script>
