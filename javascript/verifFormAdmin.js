e984939884321152d128c8d91d38732f04f96a9a
Unknown <vscavinner@gmail.com>
2018-06-13 09:42:51 +0200

    verificationFormulaire V4
    ajout de l'optimisation sur la comparaison des champs (parole et réponses)

--- javascript/verifFormAdmin.js
+++ javascript/verifFormAdmin.js
@@ -1,11 +1,11 @@
 //Fonction principale
 function verifForm(f) {
-    
-    var urlOk= verifierURL(f.url);
-    var timecodesOk= verifierTimecodes(f.timecode) ;
-    var parolesOk= verifierParoles(f.paroles);
 
-    if ( urlOk && timecodesOk && parolesOk){
+    var urlOk = verifierURL(f.url);
+    var timecodesOk = verifierTimecodes(f.timecode);
+    var parolesOk = verifierParoles(f.paroles);
+
+    if (urlOk && timecodesOk && parolesOk) {
         return true;
     } else {
         alert("Attention à bien remplir correctement tous les champs");
@@ -34,7 +34,7 @@ function verifierTimecodes(champ) {
     var s1 = document.getElementById("secStart_1").value;
     var s2 = document.getElementById("secEnd_1").value;
 
-    if ((m2 < m1) || (m1=m2 && s2<s1)) {
+    if ((m2 < m1) || (m1 = m2 && s2 < s1)) {
         surligne(m1, true);
         surligne(m2, true);
         surligne(s1, true);
@@ -48,20 +48,31 @@ function verifierTimecodes(champ) {
 
 //Fonction vérifiant que les paroles et réponses sont différentes pour chaque champ
 function verifierParoles(champ) {
-    
+
     var p1 = champ.value.toLowerCase();
     var p2 = document.getElementById("goodRep_1").value.toLowerCase();
     var p3 = document.getElementById("badRep1_1").value.toLowerCase();
     var p4 = document.getElementById("badRep2_1").value.toLowerCase();
     var p5 = document.getElementById("badRep3_1").value.toLowerCase();
-    
-    if(p1==p2 || p1==p3 || p1==p4 || p1==p5 || p2==p3 || p2==p4 || p2==p5 || p3==p4 || p3==p5 || p4==p5){
-        surligne(p1, true);
-        surligne(p2, true);
-        surligne(p3, true);
-        surligne(p4, true);
-        surligne(p5, true);
-    } else {
+
+    var tabParoles = [p1, p2, p3, p4, p5];
+
+    function comparer(tabParoles) {
+        for (var paroles of tabParoles) {
+            let array = [];
+            for (var p of paroles) {
+                if (p !== paroles) {
+                    array.push(p);
+                }
+            }
+            if (array.length !== valeurs.length - 1) {
+                surligne(p1, true);
+                surligne(p2, true);
+                surligne(p3, true);
+                surligne(p4, true);
+                surligne(p5, true);
+            }
+        }
         return true;
     }
 
