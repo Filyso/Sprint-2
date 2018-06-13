6916eacb3cd0ccb2e1527c80b33a3363c0141c3f
Unknown <yoyan.bid@gmail.com>
2018-06-13 11:31:41 +0200

    Save Final

--- php/modify_song_admin.php
+++ php/modify_song_admin.php
@@ -1,5 +1,25 @@
 <?php
 	header("Content-type: text/html; charset: UTF-8");
+
+    if(isset($_POST["admin"]) && $_POST["admin"] == "supprimer") {
+        try {
+            // ETAPE 1 : Se connecter au serveur de base de données
+            require("./param.inc.php");
+            $pdo = new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS);
+            $pdo->query("SET NAMES utf8");
+            $pdo->query("SET CHARACTER SET 'utf8'");
+            
+            // ETAPE 2 : Envoyer une requête SQL (demander la liste des données)
+            $requeteSQL = "DELETE FROM CHANSONS WHERE CHANSONS.idSong = :paramIdSong";
+            $statement = $pdo->prepare($requeteSQL);
+            $statement->execute(array(":paramIdSong" => $_POST["songId"]));
+            
+            // ETAPE 4 : Déconnecter du serveur
+            $pdo = null;
+        } catch (Exception $e) {
+            echo($e);
+        }
+    }
 ?>
     <section class="selectMulti">
         <table>
@@ -20,7 +40,6 @@
                             $pdo->query("SET CHARACTER SET 'utf8'");
 
                         // ETAPE 2 : Envoyer une requête SQL (demander la liste des données)
-
                             $requeteSQL = "SELECT CHANSONS.idSong, nameSong, nameArtist FROM CHANSONS INNER JOIN A_UN ON CHANSONS.idSong = A_UN.idSong INNER JOIN ARTISTES ON A_UN.idArtist = ARTISTES.idArtist ORDER BY nameSong ASC";
                             $statement = $pdo->query($requeteSQL);
 
@@ -29,17 +48,25 @@
                             while($ligne != false) {
                     ?>
                     <tr>
-                        <form action="admin.php" method="get">
-                            <td>
-                                <?php echo($ligne["nameSong"])?>
-                            </td>
-                            <td>
-                                <?php echo($ligne["nameArtist"])?>
-                            </td>
-                            <td><input type="submit" value="Modifier" name="admin" class="editSongBtn" /></td>
-                            <td><input type="submit" value="Supprimer" name="admin" class="editSongBtn" /></td>
-                            <td><input type="hidden" value="<?php echo($ligne["idSong"])?>" name="songId" /></td>
-                        </form>
+
+                        <td>
+                            <?php echo($ligne["nameSong"])?>
+                        </td>
+                        <td>
+                            <?php echo($ligne["nameArtist"])?>
+                        </td>
+                        <td>
+                            <form action="admin.php?admin=Modification%2FSuppression+de+chansons" method="post">
+                                <input type="submit" value="Modifier" name="admin" class="editSongBtn" />
+                            </form>
+                        </td>
+                        <td>
+                            <form action="admin.php?admin=Modification%2FSuppression+de+chansons" method="post">
+                                <input type="button" value="Supprimer" class="suppSongBtn" />
+                                <input type="hidden" value="supprimer" name="admin" />
+                                <input type="hidden" value="<?php echo($ligne["idSong"])?>" name="songId" />
+                            </form>
+                        </td>
                     </tr>
                     <?php
                                 $ligne = $statement->fetch(PDO::FETCH_ASSOC);
@@ -55,3 +82,4 @@
         </table>
         <input type="hidden" value="0" name="song" />
     </section>
+    <script type="text/javascript" src="../javascript/modify_song.js"></script>
