a1e9ff3da77de02d9af64db4ddd312fdf9ea4a62
Unknown <villatte.maxime@orange.fr>
2018-06-14 10:08:34 +0200

    correction détail

--- javascript/verification_conexion.js
+++ javascript/verification_conexion.js
@@ -29,7 +29,7 @@
             },
             function(data){
                 
-                console.log(data);
+                
                 if(data == "Echec"){
                     
                     $("#pseudo").val("");
