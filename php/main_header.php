5eee98f27860acf77cb34be1430c86ff995dfdc3
Unknown <clem.hyaumet@gmail.com>
2018-06-18 17:45:34 +0200

    on casse tout

--- php/main_header.php
+++ php/main_header.php
@@ -156,17 +156,7 @@
                     <button type="submit">SE CONNECTER</button>
                 </form>
             </div>
-            <div class="popup">
-                <div class="popup-content">
-                    <span class="close">&times;</span>
-                    <h2>CONNECTEZ-VOUS</h2>
-                    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="post">
-                        <input id="pseudo" type="text" required="required" name="pseudo" placeholder="Pseudo...">
-                        <input id="passwd" type="password" required="required" name="pass" placeholder="Mot de passe...">
-                        <button type="submit" id="connexionBtn">SE CONNECTER</button>
-                    </form>
-                </div>
-            </div>
+
         </header>
 
         <script src="https://kodhus.com/moveit.js"></script>
