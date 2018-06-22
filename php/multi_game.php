918557ccb85b3ce5f373815d42299a3bd91aebf6
Unknown <justineagnelet@gmail.com>
2018-06-22 14:08:41 +0200

    maj 4

--- php/multi_game.php
+++ php/multi_game.php
@@ -5,7 +5,6 @@
     <html lang="fr">
 
     <head>
-        <meta charset="utf-8">
         <meta http-equiv="" content="">
         <title>Jeux multijoueur Filyso</title>
         <meta name="description" content="Bienvenue sur le jeu multijoueur de Filyso. Vous pourrez affronter d'autres joueurs en duel pour encore plus de fun !">
